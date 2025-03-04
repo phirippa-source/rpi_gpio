a7128c77e9dc41e5087632ce50ed1f50626f43de
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-03-04 14:12:11 +0900

    Update rpi_blink.py

--- rpi_blink.py
+++ rpi_blink.py
@@ -6,7 +6,9 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(pin_led, GPIO.OUT)
 
 for i in range(10):
-    GPIO.output(pin_led, 1)
-    time.sleep(1)
     GPIO.output(pin_led, 0)
     time.sleep(1)
+    GPIO.output(pin_led, 1)
+    time.sleep(1)
+
+GPIO.cleanup()
