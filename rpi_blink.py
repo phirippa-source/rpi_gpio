690d1abc1d918841faf584127fd9dc315b4df269
김성필(Sung-Pil, Kim) <82075299+phirippa-source@users.noreply.github.com>
2025-03-04 14:09:11 +0900

    Create rpi_blink.py
    디지털 출력 예

---
+++ rpi_blink.py
@@ -0,0 +1,12 @@
+import RPi.GPIO as GPIO
+import time
+
+pin_led = 4
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin_led, GPIO.OUT)
+
+for i in range(10):
+    GPIO.output(pin_led, 1)
+    time.sleep(1)
+    GPIO.output(pin_led, 0)
+    time.sleep(1)
